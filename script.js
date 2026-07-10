ab8f65adff19df4939ed831cb37f009a7603c77d
adesuyanvictoradeyemi-hub <adesuyanvictoradeyemi@gmail.com>
2026-07-10 20:24:27 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("incidentForm").addEventListener("submit", function(e){
-
-e.preventDefault();
-
-alert("Incident submitted successfully.\n\nThank you for helping keep our campus safe.");
-
-this.reset();
-
-});

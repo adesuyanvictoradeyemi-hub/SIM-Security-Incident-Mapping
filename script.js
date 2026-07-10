ed33459275f658b23ba411ef54c7a9467c89f803
adesuyanvictoradeyemi-hub <adesuyanvictoradeyemi@gmail.com>
2026-07-10 20:20:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,109 @@
-document.getElementById("incidentForm").addEventListener("submit", function(event){
+body{
 
-event.preventDefault();
+font-family:Arial, sans-serif;
 
-alert("Incident Report Submitted Successfully!");
+margin:0;
 
-this.reset();
+background:#f2f6fc;
 
-});
+}
+
+header{
+
+background:#003366;
+
+color:white;
+
+padding:25px;
+
+text-align:center;
+
+}
+
+nav{
+
+background:#0055aa;
+
+padding:15px;
+
+text-align:center;
+
+}
+
+nav a{
+
+color:white;
+
+text-decoration:none;
+
+margin:20px;
+
+font-weight:bold;
+
+}
+
+section{
+
+background:white;
+
+width:80%;
+
+margin:25px auto;
+
+padding:20px;
+
+border-radius:10px;
+
+box-shadow:0 0 10px lightgray;
+
+}
+
+input,select,textarea{
+
+width:100%;
+
+padding:10px;
+
+margin-top:8px;
+
+margin-bottom:15px;
+
+}
+
+button{
+
+background:#0055aa;
+
+color:white;
+
+padding:12px;
+
+border:none;
+
+width:100%;
+
+font-size:16px;
+
+cursor:pointer;
+
+}
+
+button:hover{
+
+background:#003366;
+
+}
+
+footer{
+
+background:#003366;
+
+color:white;
+
+text-align:center;
+
+padding:20px;
+
+margin-top:30px;
+
+}
